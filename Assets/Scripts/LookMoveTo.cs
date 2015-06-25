b9d155ed2f38c4ebe0758a544c634a97e077b547
Jonathan Linowes <jonathan@parkerhill.com>
2015-06-25 17:28:08 -0400

    Chapter 5-6 Info Bubble

--- Assets/Scripts/LookMoveTo.cs
+++ Assets/Scripts/LookMoveTo.cs
@@ -1,25 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LookMoveTo : MonoBehaviour {
 	public GameObject ground;
+	public Transform infoBubble;
+
+	private Text infoText;
+
+	void Start() {
+		if (infoBubble != null) {
+			infoText = infoBubble.Find ("Text").GetComponent<Text> ();
+		}
+	}
 
 	void Update () {
-//		Ray ray;
-//		RaycastHit hit;
-//		GameObject hitObject;
-//
-//		Debug.DrawRay (camera.transform.position, camera.transform.rotation * Vector3.forward * 100.0f);
-//
-//		ray = new Ray (camera.transform.position, camera.transform.rotation * Vector3.forward);
-//		if (Physics.Raycast (ray, out hit)) {
-//			hitObject = hit.collider.gameObject;
-//			if (hitObject == ground) {
-//				Debug.Log ("Hit (x,y,z): " + hit.point.ToString ("F2"));
-//				transform.position = hit.point;
-//			}
-//		}	
-
 		Camera camera = Camera.main;
 		Ray ray;
 		RaycastHit[] hits;
@@ -34,6 +29,11 @@ public class LookMoveTo : MonoBehaviour {
 			hitObject = hit.collider.gameObject;
 			if (hitObject == ground) {
 				Debug.Log ("Hit (x,y,z): " + hit.point.ToString ("F2"));
+				if (infoBubble != null) {
+					infoText.text = "X:" + hit.point.x.ToString("F2") + ", Z:" + hit.point.z.ToString("F2");
+					infoBubble.LookAt(camera.transform.position);
+					infoBubble.Rotate (0.0f, 180.0f, 0.0f);
+				}
 				transform.position = hit.point;
 			}
 		}
