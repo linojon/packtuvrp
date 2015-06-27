7fb28d7ec5981b5ebd497556e85b055254ebb16a
Jonathan Linowes <jonathan@parkerhill.com>
2015-06-27 03:31:32 -0400

    Chapter 6-4 head, body, feet and nose

--- Assets/Scripts/HeadLookWalk.cs
+++ Assets/Scripts/HeadLookWalk.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class HeadLookWalk : MonoBehaviour {
 	public float velocity = 0.7f;
+
 	private CharacterController controller;
+	private AudioSource footsteps;
+	private Transform head;
+	private Transform body;
 	private HeadGesture gesture;
 	private bool walking;
 
 	void Start() {
 		controller = GetComponent<CharacterController> ();
+		footsteps = GetComponent<AudioSource> ();
+		head = Camera.main.transform.Find ("MeHead");
+		body = transform.Find ("MeBody");
 		gesture = GameObject.Find ("GameController").GetComponent<HeadGesture> ();
 		walking = false;
 	}
@@ -19,6 +26,12 @@ public class HeadLookWalk : MonoBehaviour {
 		}
 		if (walking) {
 			controller.SimpleMove (Camera.main.transform.forward * velocity);
+			body.rotation = Quaternion.Euler (new Vector3 (0.0f, head.eulerAngles.y, 0.0f));
+			if (!footsteps.isPlaying) {
+				footsteps.Play ();
+			} 
+		} else { // not walking
+			footsteps.Stop();
 		}
 	}
 	
