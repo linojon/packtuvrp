1169bb3d987d788a7301be8114a8af503516adad
Jonathan Linowes <jonathan@parkerhill.com>
2015-06-29 01:39:50 -0400

    added Clicker class

--- Assets/Scripts/HeadLookWalk.cs
+++ Assets/Scripts/HeadLookWalk.cs
@@ -6,26 +6,18 @@ public class HeadLookWalk : MonoBehaviour {
 	public float velocity = 0.7f;
 
 	private CharacterController controller;
+	private Clicker clicker = new Clicker();
 
 	void Start() {
 		controller = GetComponent<CharacterController> ();
 	}
 
 	void Update () {
-		if (keyPressed()) {
+		if (clicker.clicked()) {
 			walking = !walking;
 		}
 		if (walking) {
 			controller.SimpleMove (Camera.main.transform.forward * velocity);
 		}
 	}
-	
-	private bool keyPressed() {
-#if (UNITY_ANDROID || UNITY_IPHONE)
-		return Cardboard.SDK.CardboardTriggered;
-#else
-		return Input.anyKeyDown;
-#endif
-	}
-
 }
